e14c34d0f1782bd12ddf96a95d8920240462918b
Ken Kamijo <p000h11073@A050202067.rfg.rfgad.ricoh.com>
2016-06-08 10:35:19 +0900

    add javadoc

--- src/main/java/com/study/spring/aspect/TestAroundAspect.java
+++ src/main/java/com/study/spring/aspect/TestAroundAspect.java
@@ -9,9 +9,22 @@ import org.aspectj.lang.annotation.Aspect;
 import com.study.spring.annotation.TestAnnotation;
 import com.study.spring.annotation.Type;
 
+/**
+ * Test reflect API with Aspect
+ *
+ * @author KKamijo
+ *
+ */
 @Aspect
 public class TestAroundAspect {
 
+    /**
+     * Get procceding method and if it has {@link com.study.spring.annotation.TestAnnotation}, system output it's label
+     *
+     * @param proceedingJoinPoint
+     * @return
+     * @throws ClassNotFoundException
+     */
     @Around("execution(* com.study.spring.service.*.*(..))")
     public Object testAroundAdvice(ProceedingJoinPoint proceedingJoinPoint)
             throws ClassNotFoundException {
@@ -19,6 +32,7 @@ public class TestAroundAspect {
                 + proceedingJoinPoint.getSignature().getDeclaringTypeName());
         Class<?> clazz = Class.forName(proceedingJoinPoint.getSignature()
                 .getDeclaringTypeName());
+
         for (Method method : clazz.getDeclaredMethods()) {
             TestAnnotation annotation = method
                     .getAnnotation(TestAnnotation.class);
